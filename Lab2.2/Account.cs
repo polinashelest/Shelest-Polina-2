819a1e340b18d44b7adeafc47809eb2d6669a800
polinashelest <107417021+polinashelest@users.noreply.github.com>
2022-06-23 22:26:59 +0200

    Add files via upload

---
+++ Lab2.2/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Bank
+{
+    public class Account
+    {
+        public int Id { get; }
+        public bool Blocked { get; set; }
+        public int Balance { get; set; }
+
+        public Account(int id)
+        {
+            Id = id;
+        }
+
+        public Account(int id, bool blocked, int balance)
+        {
+            Id = id;
+            Blocked = blocked;
+            Balance = balance;
+        }
+
+    }
+}
